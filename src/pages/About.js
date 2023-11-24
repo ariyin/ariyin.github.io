46ecd818c23192a3f00aec2b0622159996ce5e4a
Jenna Wang <jenna2004@gmail.com>
2023-11-24 02:02:12 -0800

    about blurb, links, img

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import mail from '../assets/mail.png';
-import discord from '../assets/discord.png';
 import linkedin from '../assets/linkedin.png';
 import newspaper from '../assets/newspaper.png';
 import { LastFMData } from './LastFMData';
@@ -13,7 +12,7 @@ function About() {
 
                 <h3> hello! </h3>
 
-                <p style = {{marginTop: 15 + 'px', marginBottom: 25 + 'px', maxWidth: 600 + 'px'}}> i’m a second year cs major at ucla, interested mostly in front-end and ui/ux! in my spare time, i enjoy playing video games, reading manhwas and novels, and exploring whatever my latest interest might be. </p>  
+                <p style = {{marginTop: 15 + 'px', marginBottom: 25 + 'px', maxWidth: 600 + 'px'}}> i'm a second year cs major at ucla, interested in full stack (mostly front-end), ui/ux, and ui development! in my spare time, i enjoy playing video games, reading manhwas and novels, listening to kpop albums i've never heard before, and exploring whatever my latest interest might be. </p>  
 
                 <LastFMData
                     username = {'j00101010'}
@@ -22,23 +21,23 @@ function About() {
 
                 <h1 style = {{marginTop: 30 + 'px'}}> contact </h1>
 
-                <div className = "icon-text" style = {{marginBottom: 15 + 'px'}}> 
-                    <a href = "mailto:jenna2004@gmail.com" target="_blank" rel="noopener noreferrer">
-                        <img className = "icon" alt = "mail icon" src = {mail} /> 
-                    </a>
-                    <p> jenna2004@gmail.com </p>
-                </div>
+                    <div className = "icon-text" style = {{marginBottom: 15 + 'px'}}> 
+                        <span> 
+                            <img className = "icon" alt = "mail icon" src = {mail} /> 
+                            <a className = "underline-highlight" href = "mailto:jenna2004@gmail.com" target="_blank" rel="noopener noreferrer">
+                                <p> jenna2004@gmail.com </p> 
+                            </a>
+                        </span>
+                    </div>
 
-                <div className = "icon-text" style = {{marginBottom: 15 + 'px'}}> 
-                    <img className = "icon" alt = "discord icon" src = {discord} /> 
-                    <p> jayenay </p>
-                </div>
+                    <div className = "icon-text" style = {{marginBottom: 15 + 'px'}}> 
+                        <span>
+                            <img className = "icon" alt = "linkedin icon" src = {linkedin} /> 
+                            <a className = "underline-highlight" href = "https://www.linkedin.com/in/jyw42/" target="_blank" rel="noopener noreferrer">
+                                <p> /jyw42 </p>
+                            </a>
+                        </span>
 
-                <div className = "icon-text"> 
-                    <a href = "https://www.linkedin.com/in/jyw42/" target="_blank" rel="noopener noreferrer">
-                        <img className = "icon" alt = "linkedin icon" src = {linkedin} /> 
-                    </a>
-                    <p> /jyw42 </p>
                 </div>
             </div>
 
